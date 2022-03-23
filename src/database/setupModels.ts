d68c2d17c154aecec941a0e99fb979c8d9deb712
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-03-23 10:59:08 -0600

    asocciations

--- src/database/setupModels.ts
+++ src/database/setupModels.ts
@@ -7,6 +7,12 @@ function setupModels (sequelize: Sequelize) {
   User.init(UserAttributes, User.config(sequelize))
   Role.init(RoleAttributes, Role.config(sequelize))
   Image.init(ImageAttributes, Image.config(sequelize))
+
+  Role.hasMany(User, { foreignKey: 'role' })
+  Image.hasMany(User, { foreignKey: 'avatar_image' })
+
+  User.belongsTo(Role, { foreignKey: 'role' })
+  User.belongsTo(Image, { foreignKey: 'avatar_image' })
 }
 
 export default setupModels
