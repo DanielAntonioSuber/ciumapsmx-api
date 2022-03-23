3a25057c33c45e7c9563c51b782f128094a6a27b
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-03-23 12:03:32 -0600

    removed consts

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,10 +1,3 @@
 export const APP_PORT = process.env.APP_PORT ? process.env.APP_PORT : 5000
 export const APP_ENV = process.env.APP_ENV || 'dev'
 export const IS_PROD = process.env.NODE_ENV === 'production'
-export const APP_ROLES = [
-  { id: 10, name: 'admin' },
-  { id: 11, name: 'moderator' },
-  { id: 12, name: 'seller' },
-  { id: 13, name: 'tourist' }
-]
-export const AVATAR_PATH = 'api public/images/avatar.webp'
