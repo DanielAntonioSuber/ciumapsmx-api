f814fd6ccab9ff881d94c704f2124c3a716eb6b6
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-03-23 10:59:53 -0600

    controller

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express'
+
+class AuthController {
+  async signUp (req: Request, res: Response) {}
+  async signIn (req: Request, res: Response) {}
+}
+
+export default AuthController
