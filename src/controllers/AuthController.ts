9108748f3b88091e56b552a4e45103162ca19f38
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-03-23 11:48:49 -0600

    r

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express'
+import UserService from '../services/UserService'
 
 class AuthController {
+  service = new UserService()
+
   async signUp (req: Request, res: Response) {}
+
   async signIn (req: Request, res: Response) {}
 }
 
