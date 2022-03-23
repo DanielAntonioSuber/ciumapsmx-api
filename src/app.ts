3f96d5adfef3bd481aaafc48f42733cb835724fd
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-03-23 11:27:10 -0600

    images

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import { APP_PORT } from './config/app'
 import routerApi from './routes'
 import initialSetup from './utils/initialSetup'
@@ -12,6 +13,7 @@ app.set('port', APP_PORT)
 // Middlewares
 
 // Routes
-app.use('api/v1', routerApi)
+app.use('/api/v1', routerApi)
+app.use('/images', express.static(path.resolve('public/images')))
 
 export default app
