fa75606d4242ee0e0932838a6bd5e020ee4ee7bd
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-03-23 14:56:40 -0600

    jwt

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,3 +1,4 @@
 export const APP_PORT = process.env.APP_PORT ? process.env.APP_PORT : 5000
 export const APP_ENV = process.env.APP_ENV || 'dev'
 export const IS_PROD = process.env.NODE_ENV === 'production'
+export const JWT_SECRET = process.env.JWT_SECRET || 'DefaultJWT'
