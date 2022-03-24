8708a55c53427887f053929c4e87f95cd9d713bd
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-03-23 22:43:36 -0600

    lklkj

--- src/controllers/PlaceController.ts
+++ src/controllers/PlaceController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import PlaceService from '../services/PlaceService'
 
 class PlaceController {
+  
   service = new PlaceService()
   createPlace = async (req: Request, res: Response) => {
     console.log(req.body)
