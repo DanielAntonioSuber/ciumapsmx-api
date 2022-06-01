ade957f0b6c947446913c45a8f99da91e3039012
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2022-06-01 00:06:27 -0500

    faqs

--- src/database/data/setData.ts
+++ src/database/data/setData.ts
@@ -4,17 +4,25 @@
 // import { Image } from '../models/Image'
 // import { Place } from '../models/Place'
 // import { ImageOfPlace } from '../models/ImageOfPlace'
+import { FAQ } from '../models/FAQ'
+import faqs from './FAQS.json'
 
 function setData () {
-  //   images.forEach(({ id, name, path }) => {
-  //     Image.create({ name, path, id })
-  //   })
-  //   places.forEach(({ description, direction, id, kind, name }) => {
-  //     Place.create({ description, direction, name, id, kind })
-  //   })
-  //   imagesOfPlaces.forEach(({ imageId, id, placeId }) => {
-  //     ImageOfPlace.create({ imageId, placeId, id })
-  //   })
+  // images.forEach(({ id, name, path }) => {
+  //   Image.create({ name, path, id })
+  // })
+
+  // places.forEach(({ description, direction, id, kind, name }) => {
+  //   Place.create({ description, direction, name, id, kind, userId: 1 })
+  // })
+
+  // imagesOfPlaces.forEach(({ imageId, id, placeId }) => {
+  //   ImageOfPlace.create({ imageId, placeId, id })
+  // })
+
+  faqs.forEach(({ answer, id, question }) => {
+    FAQ.findOrCreate({ where: { id }, defaults: { answer, question } })
+  })
 }
 
 export default setData
